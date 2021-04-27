01b4ed5a125ecd9d0cd881e3e505f7a8627ef145
leo-guinan <leo@guinantech.com>
2021-04-27 09:28:02 -0400

    adding default export to product detail page

---
+++ pages/products/[id].js
@@ -0,0 +1,17 @@
+export default function ProductDetails({ product }) {
+    console.log(product)
+    return <div>
+        This is the detailed product component.
+    </div>>
+
+}
+
+
+export async function getStaticProps(context) {
+    return {
+      props: {
+          product: {}    
+          
+      }, // will be passed to the page component as props
+    }
+  }
